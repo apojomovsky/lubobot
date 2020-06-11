a4d3ee272d0d7e6c7d359f4f760d93909deeb9a9
Alexis Pojomovsky <apojomovsky@gmail.com>
2020-06-10 23:09:10 -0300

    Working cmd_vel through ROS

--- firmware/proyecto_unificado/Src/maincpp.cpp
+++ firmware/proyecto_unificado/Src/maincpp.cpp
@@ -7,10 +7,11 @@
 #include <main.h>
 #include <maincpp.h>
 
+#include <geometry_msgs/Twist.h>
 #include <std_msgs/String.h>
 #include <ros.h>
 
-const TickType_t xDelay = 500 / portTICK_PERIOD_MS;
+const TickType_t xDelay = 200 / portTICK_PERIOD_MS;
 
 extern UART_HandleTypeDef huart5;
 irobot::create2 robot(&huart5, BRC_GPIO_Port, BRC_Pin);
@@ -19,6 +20,19 @@ ros::NodeHandle nh;
 std_msgs::String str_msg;
 ros::Publisher chatter("chatter", &str_msg);
 char hello[] = "Hello world!";
+
+geometry_msgs::Twist cmdvel_msg;
+
+float vel_right, vel_left;
+const float wheels_dist = 235.0;
+
+void cmdvel_cb(const geometry_msgs::Twist& msg) {
+	vel_right = msg.linear.x + msg.angular.z * wheels_dist / 2;
+	vel_left = msg.linear.x - msg.angular.z * wheels_dist / 2;
+	robot.driveVelocity(vel_right, vel_left);
+}
+
+ros::Subscriber<geometry_msgs::Twist> cmdvel_sub("cmd_vel", &cmdvel_cb);
 
 void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart) {
 	nh.getHardware()->flush();
@@ -31,6 +45,8 @@ void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
 void setup(void) {
 	nh.initNode();
     nh.advertise(chatter);
+    nh.subscribe(cmdvel_sub);
+
 	robot.start();
 	vTaskDelay(500);
 	robot.pauseStream();
