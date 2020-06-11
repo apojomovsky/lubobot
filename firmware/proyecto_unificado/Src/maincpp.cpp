fbb5ed0a7ef23789269dc6e58ad2c783960a7ef2
Alexis Pojomovsky <apojomovsky@gmail.com>
2020-06-10 22:48:01 -0300

    FreeRTOS + rosserial + create2

--- firmware/proyecto_unificado/Src/maincpp.cpp
+++ firmware/proyecto_unificado/Src/maincpp.cpp
@@ -1,22 +1,21 @@
-#include <cmsis_os.h>
-#include <main.h>
-#include <ros.h>
 #include <stdio.h>
 #include <string.h>
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+#include <cmsis_os.h>
 
+#include <create2.h>
+#include <main.h>
 #include <maincpp.h>
-#include <std_msgs/String.h>
 
-const TickType_t xDelay = 1000 / portTICK_PERIOD_MS;
+#include <std_msgs/String.h>
+#include <ros.h>
+
+const TickType_t xDelay = 500 / portTICK_PERIOD_MS;
 
 extern UART_HandleTypeDef huart5;
+irobot::create2 robot(&huart5, BRC_GPIO_Port, BRC_Pin);
 
 ros::NodeHandle nh;
-
 std_msgs::String str_msg;
 ros::Publisher chatter("chatter", &str_msg);
 char hello[] = "Hello world!";
@@ -32,6 +31,19 @@ void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
 void setup(void) {
 	nh.initNode();
     nh.advertise(chatter);
+	robot.start();
+	vTaskDelay(500);
+	robot.pauseStream();
+	vTaskDelay(500);
+	robot.goSafeMode();
+	vTaskDelay(500);
+	robot.driveVelocity(-64, 64);
+	vTaskDelay(1000);
+	robot.driveVelocity(0, 0);
+	vTaskDelay(500);
+	robot.driveVelocity(64, -64);
+	vTaskDelay(1000);
+	robot.driveVelocity(0, 0);
 }
 
 void loop(void) {
@@ -41,7 +53,3 @@ void loop(void) {
     nh.spinOnce();
 	vTaskDelay(xDelay);
 }
-
-#ifdef __cplusplus
-}
-#endif
