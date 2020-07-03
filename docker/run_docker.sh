b86f89060004af6e8931d43acfa520655a8848cc
Alexis Pojomovsky <apojomovsky@gmail.com>
2020-07-03 16:33:17 -0300

    WIP: Added robot+kinect description

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -14,16 +14,18 @@ DOCKER_MOUNT_ARGS="\
     -v ${REPO_DIR}/lubobot_msgs:/catkin_ws/src/lubobot_msgs"
 DOCKER_GRAPHICS_FLAG="--device /dev/dri"
 DOCKER_ULIMIT_ARGS="--ulimit core=-1"
+REMOTE_ARGS=""
+# REMOTE_ARGS="-e ROS_MASTER_URI=http://192.168.1.200:11311 \
+#         -e ROS_HOSTNAME=192.168.1.200 \
+#         -e ROS_IP=192.168.1.200"
 
 xhost +
 docker run ${DOCKER_ULIMIT_ARGS} --privileged --rm \
         ${DOCKER_CAPABILITIES} \
         ${DOCKER_MOUNT_ARGS} \
         -v /etc/fstab:/etc/fstab:ro \
-        -e ROS_MASTER_URI=http://192.168.1.200:11311 \
-        -e ROS_HOSTNAME=192.168.1.200 \
-        -e ROS_IP=192.168.1.200 \
         ${DOCKER_GRAPHICS_FLAG} \
+        ${REMOTE_ARGS} \
         --device /dev/ttyACM0 \
         --device /dev/ttyUSB0 \
         ${DOCKER_NETWORK} \
